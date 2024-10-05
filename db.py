e18e2160c7b7304f067b49a7c8df787bf8f0cb0b
Devraj Kashyap <96662811+dev-rajk@users.noreply.github.com>
2024-10-05 19:39:35 +0530

    Update db.py

--- db.py
+++ db.py
@@ -40,7 +40,7 @@ def add_event(quiz_name, date, time, category, venue, location, organizer, genre
     })
 
 def get_events(status):
-    events_ref = db.collection('events').document().where(filter=FieldFilter('status', '==', status))
+    events_ref = db.collection('events').where('status', '==', status)
     events = events_ref.stream()
     events_list = []
     for event in events:
