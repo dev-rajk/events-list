13694f23049a449605f48b3426af3eecaf59b4f3
Devraj Kashyap <96662811+dev-rajk@users.noreply.github.com>
2024-10-14 20:28:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -229,22 +229,22 @@ if choice == "View Events":
         st.components.v1.html(fullcalendar(events, theme_mode=theme_mode), height=800)
 
         st.markdown('''
-                :red[<em>This website is free of cost for all users. \n
+                :red[ **This website is free of cost for all users. \n
                 The maintainers bear no responsibility for the accuracy of the event information, or any loss or damages resulting from the use of this site. The maintainer is not liable for any damages or misinformation. \n
                 Users are encouraged to verify all event information. \n
                 No personal infomation is tracked. \n
-                Cheers to quizzing.</em>] \n
+                Cheers to quizzing.** ] \n
                   ''')
 
             
     else:
         st.write("No approved events to show.")
         st.markdown('''
-                :red[<em>This website is free of cost for all users. \n
+                :red[ **This website is free of cost for all users. \n
                 The maintainers bear no responsibility for the accuracy of the event information, or any loss or damages resulting from the use of this site. The maintainer is not liable for any damages or misinformation.  \n
                 Users are encouraged to verify all event information. \n
                 No personal infomation is tracked. \n
-                Cheers to quizzing.</em>] \n
+                Cheers to quizzing.** ] \n
                   ''')
 
 
@@ -280,11 +280,11 @@ elif choice == "Submit Event":
             
 
     st.markdown('''
-                :red[<em>This website is free of cost for all users. \n
+                :red[ **This website is free of cost for all users. \n
                 The maintainers bear no responsibility for the accuracy of the event information, or any loss or damages resulting from the use of this site. The maintainer is not liable for any damages or misinformation.  \n
                 Users are encouraged to verify all event information. \n
                 No personal infomation is tracked. \n
-                Cheers to quizzing.</em>] \n
+                Cheers to quizzing.** ] \n
                   ''')
 
 
